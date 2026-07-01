bc7c79f7f7f6f6518e94f46db835b5457a85b1d3
tapframe <85391825+tapframe@users.noreply.github.com>
2026-07-01 20:34:49 +0530

    Revert "Merge branch 'switchdb' into cmp-rewrite"
    This reverts commit e883d890312bea9b775fc06061ecaf49ae0c896f, reversing
    changes made to 6885bbdee71a6b526d82160c7c901ce8494fcfb2.

--- composeApp/src/commonMain/kotlin/com/nuvio/app/core/network/SupabaseProvider.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/core/network/SupabaseProvider.kt
@@ -1,7 +1,6 @@
 package com.nuvio.app.core.network
 
 import com.nuvio.app.core.build.AppVersionConfig
-import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.annotations.SupabaseInternal
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
@@ -11,34 +10,12 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.http.HttpHeaders
 
 object SupabaseProvider {
-    private data class ClientHolder(
-        val backend: SyncBackendConfig,
-        val client: SupabaseClient,
-    )
-
-    private var holder: ClientHolder? = null
-
-    val selectedBackend: SyncBackendConfig
-        get() = SyncBackendRepository.selectedBackend
-
     @OptIn(SupabaseInternal::class)
-    val client: SupabaseClient
-        get() = clientFor(selectedBackend)
-
-    fun rebuildClient() {
-        holder = null
-    }
-
-    @OptIn(SupabaseInternal::class)
-    private fun clientFor(config: SyncBackendConfig): SupabaseClient {
-        holder
-            ?.takeIf { it.backend.hasSameConnectionIdentity(config) }
-            ?.let { return it.client }
-
+    val client by lazy {
         val userAgent = "NuvioMobile/${AppVersionConfig.VERSION_NAME.ifBlank { "dev" }}"
-        val nextClient = createSupabaseClient(
-            supabaseUrl = config.normalizedSupabaseUrl,
-            supabaseKey = config.anonKey,
+        createSupabaseClient(
+            supabaseUrl = SupabaseConfig.URL,
+            supabaseKey = SupabaseConfig.ANON_KEY,
         ) {
             httpConfig {
                 defaultRequest {
@@ -49,7 +26,5 @@ object SupabaseProvider {
             install(Postgrest)
             install(Functions)
         }
-        holder = ClientHolder(backend = config, client = nextClient)
-        return nextClient
     }
 }
